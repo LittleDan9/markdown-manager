1353da93e5a7b87152b65e666e919a75b00504be
Daniel R. Little <dan@littledan.com>
2025-09-03 21:57:55 -0400

    cleanup

--- frontend/src/components/file/index.js
+++ frontend/src/components/file/index.js
@@ -3,4 +3,3 @@ export { default as FileImportModal } from './FileImportModal.jsx';
 export { default as FileOpenModal } from './FileOpenModal.jsx';
 export { default as FileOverwriteModal } from './FileOverwriteModal.jsx';
 export { default as FileSaveAsModal } from './FileSaveAsModal.jsx';
-export { default as GitHubFileBrowser } from './GitHubFileBrowser.jsx';
